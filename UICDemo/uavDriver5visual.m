87e45f3afa1615aab68d671cdbd7a905019fa878
pisihara <summerresearch14@gmail.com>
2018-11-07 14:58:36 -0600

    Add files via upload

--- UICDemo/uavDriver5visual.m
+++ UICDemo/uavDriver5visual.m
@@ -45,7 +45,7 @@ zoneLocations = [276,715;
                  635 728];
 zoneParam = [zoneLocations,zonesXProb,zonesYProb,exprTimeX,exprTimeY]; 
 
-numsimsteps = 2; %% number of hours to plot UAV routes
+numhrs = 2; %% number of hours to plot UAV routes
 % Color array for the UAV's
 color = ['y', 'c','m','b','r','w','k','g','y','c','m','b','r','w','k','g'];
 % Read in map for background of graph (use file name of image for map)
@@ -55,7 +55,7 @@ MAP=imread('Map2.png'); image(MAP);
 hold on
 plot(base(1,1),base(1,2),'ro','MarkerFaceColor','r')
 hold on
-for step=1:numsimsteps
+for step=1:numhrs
 %% Run Simulation
 % Run the simulation and display UAV paths
 [~, ~, ~, ~,~, manager]=uavSim3visual(uavVector, zoneParam, base, priFac,timeFac,addedVal, duration,km2pixRatio);
